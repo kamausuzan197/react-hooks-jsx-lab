b9ad21dc8a4c92d7ceb55e5ea4c969014bf8da44
kamausuzan197 <kamaususan197@gmail.com>
2022-07-13 11:15:44 +0300

    working with jsx

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div>
+    <h2>About Me</h2>
+    <p>i love coding.</p>
+    <img src="./logo512.png" alt="I made this"></img>
+  </div>;
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
+  <nav>
+  <a href="#home">Home</a>,
+  <a href="#about">About</a>
+  </nav>
   return <nav>NavBar</nav>;
+
 }
 
 export default NavBar;
